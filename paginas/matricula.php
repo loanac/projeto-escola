d30f01615de48231373ffa3fd68bfd5b8d19d585
loanac <lcavalcantis07@gmail.com>
2020-10-21 20:16:06 -0300

    luana-alteraçoes

--- paginas/matricula.php
+++ paginas/matricula.php
@@ -1,150 +1,165 @@
-<body>
-
-    <div class="container">
-        <section class="matric1">
-            <div class="row">
-                <div class="col text-center">
-                    <img class="w-50" src="public/img/iconfinderclock.png" alt="">
-                </div>
-                <div class="col">
-                    <br>
-                    <h2 class="text-light">ALUNOS VETERANOS</h2>
-                    <p class="text-light">02 de novembro de 2020 a 29 de novembro de 2020</p>
-                    <br>
-                    <br>
-                    <br>
-                    <h2 class="text-light">ALUNOS NOVATOS</h2>
-                    <p class="text-light">01 de novembro de 2020 a 30 de dezembro de 2020</p>
-                </div>
-            </div>
-        </section>
-    </div>
-    <div class="container">
-        <section class="sec2">
-            <br>
-            <h1 class="text-center">Procedimentos para Matrícula</h1>
-            <br>
-            <div class="row">
-                <div class="col">
-                    <ul>
-                        <li> Ficha de Cadastro preenchida;</li>
-                        <li> Ficha médica do aluno;</li>
-                        <li> Comprovante de residência;</li>
-                        <li> Cópia do CPF, RG (Responsável financeiro)</li>
-                        <li> Atestado de aptidão física (a partir do 1º ano do ensino fundamental) </li>
-                    </ul>
-                </div>
-                <div class="col">
-                    <img class="w-75" src="public/img/ilustracoes.png" alt="">
-                </div>
-                <div class="col">
-                    <ul>
-                        <li> Cópia do RG ou registro de nascimento do aluno;</li>
-                        <li> Histórico ou declaração provisória (aluno novato);</li>
-                        <li> Foto 3x4;</li>
-                        <li> Assinatura do contrato de prestação de serviços educacionais 2020.</li>
-                    </ul>
-                </div>
-            </div>
-        </section>
-    </div>
-    <!-- valores de mat -->
-    <div class="container">
+<div class="container">
+    <section class="matric1">
         <br>
-        <h1 class="text-center">Valores de matrícula 2020</h1>
+        <h1 class="textanima">
+            <span>m</span>
+            <span>a</span>
+            <span>t</span>
+            <span>r</span>
+            <span>í</span>
+            <span>c</span>
+            <span>u</span>
+            <span>l</span>
+            <span>a</span>
+            <span>s</span>
+            <span class="space"></span>
+            <span>2</span>
+            <span>0</span>
+            <span>2</span>
+            <span>0</span>
+        </h1>
+        <div class="row">
+            <div class="col text-center">
+                <img class="w-50" src="public/img/iconfinderclock.png" alt="">
+            </div>
+            <div class="col">
+                <br>
+                <h2 class="text-light">ALUNOS VETERANOS</h2>
+                <p class="text-light">02 de novembro de 2020 a 29 de novembro de 2020</p>
+                <br>
+                <br>
+                <br>
+                <h2 class="text-light">ALUNOS NOVATOS</h2>
+                <p class="text-light">01 de novembro de 2020 a 30 de dezembro de 2020</p>
+            </div>
+        </div>
+    </section>
+</div>
+<div class="container">
+    <section class="sec2">
+        <br>
+        <h1 class="text-center">Procedimentos para Matrícula</h1>
         <br>
         <div class="row">
             <div class="col">
-                <div class="card border-danger mb-3" style="max-width: 18rem;">
-                    <div class="card-header"> <strong>Educação Infantil</strong> <small>(I ano)</small> </div>
-                    <div class="card-body text-danger">
-                        <p class="card-text text-center"><small>R$</small> <strong>420</strong> <small>00</small></p>
-                    </div>
-                </div>
+                <ul>
+                    <li> Ficha de Cadastro preenchida;</li>
+                    <li> Ficha médica do aluno;</li>
+                    <li> Comprovante de residência;</li>
+                    <li> Cópia do CPF, RG (Responsável financeiro)</li>
+                    <li> Atestado de aptidão física (a partir do 1º ano do ensino fundamental) </li>
+                </ul>
             </div>
             <div class="col">
-                <div class="card border-danger mb-3" style="max-width: 18rem;">
-                    <div class="card-header"> <strong>Ensino Fundamental I</strong> <small>(1º ao 5º ano)</small></div>
-                    <div class="card-body text-danger">
-                        <p class="card-text text-center"><small>R$</small> <strong>400</strong> <small>00</small></p>
-                    </div>
-                </div>
+                <img class="w-75" src="public/img/ilustracoes.png" alt="">
             </div>
             <div class="col">
-                <div class="card border-danger mb-3" style="max-width: 18rem;">
-                    <div class="card-header"> <strong>Ensino Fundamental II <small>(6º ano)</small></strong></div>
-                    <div class="card-body text-danger">
-                        <p class="card-text text-center"><small>R$</small> <strong>420</strong> <small>00</small></p>
-                    </div>
+                <ul>
+                    <li> Cópia do RG ou registro de nascimento do aluno;</li>
+                    <li> Histórico ou declaração provisória (aluno novato);</li>
+                    <li> Foto 3x4;</li>
+                    <li> Assinatura do contrato de prestação de serviços educacionais 2020.</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</div>
+<!-- valores de mat -->
+<div class="container">
+    <br>
+    <h1 class="text-center">Valores de matrícula 2020</h1>
+    <br>
+    <div class="row">
+        <div class="col">
+            <div class="card border-danger mb-3" style="max-width: 18rem;">
+                <div class="card-header"> <strong>Educação Infantil</strong> <small>(I ano)</small> </div>
+                <div class="card-body text-danger">
+                    <p class="card-text text-center"><small>R$</small> <strong>420</strong> <small>00</small></p>
                 </div>
             </div>
         </div>
-
-        <!-- coluna 2 -->
-
-        <div class="row">
-            <div class="col">
-                <div class="card border-danger mb-3" style="max-width: 18rem;">
-                    <div class="card-header"> <strong>Educação Infantil</strong> <small>(II ao IV ano)</small> </div>
-                    <div class="card-body text-danger">
-                        <p class="card-text text-center"><small>R$</small> <strong>420</strong> <small>00</small></p>
-                    </div>
+        <div class="col">
+            <div class="card border-danger mb-3" style="max-width: 18rem;">
+                <div class="card-header"> <strong>Ensino Fundamental I</strong> <small>(1º ao 5º ano)</small></div>
+                <div class="card-body text-danger">
+                    <p class="card-text text-center"><small>R$</small> <strong>400</strong> <small>00</small></p>
                 </div>
             </div>
-            <div class="col">
-
-            </div>
-            <div class="col">
-                <div class="card border-danger mb-3" style="max-width: 18rem;">
-                    <div class="card-header"> <strong>Ensino Fundamental II</strong><small> (7º ao 9º)</small></div>
-                    <div class="card-body text-danger">
-                        <p class="card-text text-center"><small>R$</small> <strong>420</strong> <small>00</small></p>
-                    </div>
+        </div>
+        <div class="col">
+            <div class="card border-danger mb-3" style="max-width: 18rem;">
+                <div class="card-header"> <strong>Ensino Fundamental II <small>(6º ano)</small></strong></div>
+                <div class="card-body text-danger">
+                    <p class="card-text text-center"><small>R$</small> <strong>420</strong> <small>00</small></p>
                 </div>
             </div>
         </div>
     </div>
 
-    <!-- lista de materiais -->
-    <div class="container">
-        <h1 class="text-center"> Lista de materiais</h1>
-        <p class="text-center textodown"> <small> <strong>(clique para baixar!)</strong> </small></p>
+    <!-- coluna 2 -->
 
-        <div class="row">
-            <div class="col">
-                <div class="card text-white bg-danger mb-3 rounded-circle" style="max-width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title text-center"><i class="fas fa-cloud-download-alt"></i></h5>
-                        <p class="card-text text-center"> <strong> Infantil I</strong></p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="card text-white bg-danger mb-3 rounded-circle" style="max-width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title text-center"><i class="fas fa-cloud-download-alt"></i></h5>
-                        <p class="card-text text-center"> <strong> Infantil II</strong></p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="card text-white bg-danger mb-3 rounded-circle" style="max-width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title text-center"><i class="fas fa-cloud-download-alt"></i></h5>
-                        <p class="card-text text-center"> <strong> Infantil III</strong></p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="card text-white bg-danger mb-3 rounded-circle" style="max-width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title text-center"><i class="fas fa-cloud-download-alt"></i></h5>
-                        <p class="card-text text-center"> <strong> Infantil IV</strong></p>
-                    </div>
+    <div class="row">
+        <div class="col">
+            <div class="card border-danger mb-3" style="max-width: 18rem;">
+                <div class="card-header"> <strong>Educação Infantil</strong> <small>(II ao IV ano)</small> </div>
+                <div class="card-body text-danger">
+                    <p class="card-text text-center"><small>R$</small> <strong>420</strong> <small>00</small></p>
                 </div>
             </div>
         </div>
-</body>
+        <div class="col">
+
+        </div>
+        <div class="col">
+            <div class="card border-danger mb-3" style="max-width: 18rem;">
+                <div class="card-header"> <strong>Ensino Fundamental II</strong><small> (7º ao 9º)</small></div>
+                <div class="card-body text-danger">
+                    <p class="card-text text-center"><small>R$</small> <strong>420</strong> <small>00</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- lista de materiais -->
+<div class="container">
+    <h1 class="text-center"> Lista de materiais</h1>
+    <p class="text-center textodown"> <small> <strong>(clique para baixar!)</strong> </small></p>
+
+    <div class="row">
+        <div class="col">
+            <div class="card text-white bg-danger mb-3 rounded-circle" style="max-width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title text-center"><i class="fas fa-cloud-download-alt"></i></h5>
+                    <p class="card-text text-center"> <strong> Infantil I</strong></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card text-white bg-danger mb-3 rounded-circle" style="max-width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title text-center"><i class="fas fa-cloud-download-alt"></i></h5>
+                    <p class="card-text text-center"> <strong> Infantil II</strong></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card text-white bg-danger mb-3 rounded-circle" style="max-width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title text-center"><i class="fas fa-cloud-download-alt"></i></h5>
+                    <p class="card-text text-center"> <strong> Infantil III</strong></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card text-white bg-danger mb-3 rounded-circle" style="max-width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title text-center"><i class="fas fa-cloud-download-alt"></i></h5>
+                    <p class="card-text text-center"> <strong> Infantil IV</strong></p>
+                </div>
+            </div>
+        </div>
+    </div>
